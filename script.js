172db87655254ccbfca4f780664121c6f23195b1
Heinz Hanrich <heinzhanrich@bluewin.ch>
2023-12-07 12:16:30 +0100

    prettier

--- script.js
+++ script.js
@@ -5,8 +5,31 @@ export function aufgabe01(args) {
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
     if (currentElement != "e")
-    if (currentElement != "E")
-    result.push(currentElement)
+      if (currentElement != "E") result.push(currentElement)
   }
   return result.join("")
+}
+
+export function aufgabe02(args) {
+  return args.toUpperCase()
+}
+
+export function aufgabe03(args) {
+  const input = args
+  let result = 0
+
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      // result = result + 1
+      // result += 1
+      result++
+    }
+  }
+  return result
+}
+for (let e = 0; e < input.length; e++) {
+  const currentElement = input[e]
+  if (currentElement === "e") {
+  }
 }
